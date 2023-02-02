f7e74b7e985aa0fd29f03267f6f95f31c94c7f05
krapjost <krapjost@gmail.com>
2023-02-03 07:26:12 +0900

    change react to solid

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    format: 'es',
+    dir: 'dist'
+  },
+  plugins: [typescript(), nodeResolve()]
+};
